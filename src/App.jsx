d85ce88455ed0b52fdd0df44a846e807de839881
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-20 21:01:13 +0530

    ci: import SearchForm & DevPortfolio components and use them

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import SearchForm from "./components/SearchForm";
+import DevPortfolio from "./components/DevPortfolio";
+
 export default function App() {
   return (
-    <div className="app-wrapper">
-      <h1>devlooker</h1>
-    </div>
+    <main className="app-wrapper">
+      <header>
+        <h1>devlooker</h1>
+      </header>
+
+      <SearchForm />
+      <DevPortfolio />
+    </main>
   );
 }
