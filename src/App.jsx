c99cb21af2c4db8f92cb198b60fb2ab222381807
Rahulkumar <rahulkumar109422@gmail.com>
2023-07-22 13:52:39 +0530

    feat: create states for username & functions for handling username

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
+
 import SearchForm from "./components/SearchForm";
 import DevPortfolio from "./components/DevPortfolio";
 
+import useFetch from "./hooks/useFetch";
+
 export default function App() {
+  const [username, setUsername] = useState("rahulbaran");
+  const [user, setUser] = useState("");
+
+  const userInfo = useFetch(username);
+
+  const handleUser = e => setUser(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUsername(user);
+    setUser("");
+  };
+
   return (
     <main className="app-wrapper">
       <header>
         <h1>devlooker</h1>
       </header>
 
-      <SearchForm />
-      <DevPortfolio />
+      <SearchForm
+        user={user}
+        handleUser={handleUser}
+        handleSubmit={handleSubmit}
+      />
+
+      {userInfo.status === "success" ? (
+        <DevPortfolio userInfo={userInfo.response} />
+      ) : (
+        <div>
+          <h2>User not found</h2>
+        </div>
+      )}
     </main>
   );
 }
